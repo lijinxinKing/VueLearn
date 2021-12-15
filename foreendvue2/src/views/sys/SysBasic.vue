b6fae7ef1efcfd8c6976750092c47acb95f77cf6
jinxin <jinxin_v@sina.com>
2021-12-15 23:07:13 +0800

    add sys basic

--- foreendvue2/src/views/sys/SysBasic.vue
+++ foreendvue2/src/views/sys/SysBasic.vue
@@ -1,23 +1,36 @@
 <template>
-<div>
-    <el-tabs v-model="activeName" type="card">
-        <el-tab-pane label="部门管理" name="depmana"></el-tab-pane>
-        <el-tab-pane label="职位管理" name="posmana"></el-tab-pane>
-        <el-tab-pane label="职称管理" name="JoblevelMana"></el-tab-pane>
-        <el-tab-pane label="奖惩规则" name="ecmana"></el-tab-pane>
-        <el-tab-pane label="权限组" name="permissmana"></el-tab-pane>
-</el-tabs>
-</div>
+    <div>
+        <el-tabs v-model="activeName" type="card">
+            <el-tab-pane label="部门管理" name="DepMana"><DepMana></DepMana></el-tab-pane>
+            <el-tab-pane label="职位管理" name="PosMana"><PosMana></PosMana></el-tab-pane>
+            <el-tab-pane label="职称管理" name="JoblevelMana"><JoblevelMana></JoblevelMana></el-tab-pane>
+            <el-tab-pane label="权限组" name="PermissMana"><PermissMana></PermissMana></el-tab-pane>
+        </el-tabs>
+    </div>
 </template>
+
 <script>
-export default {
-    name: "SysBasic",
-    data(){
-        return{
-            activeName:'PosMana'
+    import DepMana from '../../components/sys/basic/DepMana'
+    import JoblevelMana from '../../components/sys/basic/JoblevelMana'
+    import PermissMana from '../../components/sys/basic/PermissMana'
+    import PosMana from '../../components/sys/basic/PosMana'
+
+    export default {
+        name: "SysBasic",
+        data() {
+            return {
+                activeName: 'DepMana'
+            }
+        },
+        components:{
+            DepMana,
+            JoblevelMana,
+            PermissMana,
+            PosMana
         }
     }
-}
 </script>
-<style>
+
+<style scoped>
+
 </style>
